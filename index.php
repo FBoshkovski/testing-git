9d99a64f2ec739dec1e0af8536fbc76edda72db1
FBoshkovsky <filipevengard13@hotmail.com>
2021-09-22 20:01:19 +0200

    some changes

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+   <?php
+
+        $open_paragraph = '<p>';
+        $theparagraph = 'this is a paragraph';
+        $close_paragraph = '</p>';
+
+        echo  $open_paragraph;
+        echo  $theparagraph;
+        echo  $close_paragraph;
+
+        echo $open_paragraph . $theparagraph . " " ."for me" . $close_paragraph;
+
+
+
+   ?>
+</body>
+</html>
